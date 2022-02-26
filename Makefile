b9cb9a57715a695ec3a45810a34fc99c1a4f40eb
-e <quartenium@gmail.com>
2022-02-26 17:58:07 +0300

    [B] [add] test items api, [fix] backend

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ reload:
 	cd D:/dev/nginx-1.21.1; ./nginx -s reload
 
 backend-dev-run:
-	symfony server:start
+	cd back; symfony server:start
---
+++ back/src/Controller/Items.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MyItemsController {
+
+
+  public function items(): Response {
+    $list = ['test', 'values'];
+    return new Response(json_encode($list));
+  }
+
+  /** 
+   * @Route("/itemsByName")
+  */
+  public function itemsByName(): Response {
+    $list = ['test', 'values']; // from db
+    return new Response(json_encode($list));
+  }
+
+}
+
+
+?>
