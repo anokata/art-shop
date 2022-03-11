b770e417292f725ef902be6679a176891063e10e
anokata <quartenium@gmail.com>
2022-03-11 13:56:23 +0300

    [add] custom input template to component

--- front/src/app/test-custom-template-ui/test-custom-template-ui.component.ts
+++ front/src/app/test-custom-template-ui/test-custom-template-ui.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-test-custom-template-ui',
   templateUrl: './test-custom-template-ui.component.html',
-  styleUrls: ['./test-custom-template-ui.component.scss']
+  styleUrls: ['./test-custom-template-ui.component.scss'],
 })
 export class TestCustomTemplateUiComponent implements OnInit {
+  totalEstimate = 10;
+  ctx = {
+    estimate: this.totalEstimate,
+    buttonText: 'TestCustomTemplateUiComponent',
+  };
 
-  constructor() { }
+  @ViewChild('button') buttonTemplate!: TemplateRef<any>;
+  @Input() customTemplate!: TemplateRef<any>;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    console.log(this.buttonTemplate);
+    console.log(this.customTemplate);
   }
-
 }
